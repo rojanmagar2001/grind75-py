53ba492ee5439ffcac1a2d1310336f2ab9a1f86b
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-05-07 11:32:52 +0545

    added valid palindrome

---
+++ week1/valid_palindrome.py
@@ -0,0 +1,9 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = re.sub(r'[^a-z]', '', s).lower() # -> O(n)
+
+        reversed_s = clean_s[::-1] # -> O(n)
+
+        return clean_s == reversed_s
