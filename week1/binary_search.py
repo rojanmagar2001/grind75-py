1e8a7e5fb9ba8d008b5b08447addb89301144a6e
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-05-07 14:53:51 +0545

    added binary search solution

--- week1/binary_search.py
+++ week1/binary_search.py
@@ -7,18 +7,17 @@ class Solution:
         right = len(nums) - 1
 
         while left <= right:
-            # Calculate the middle index
-            mid = left + (left - right) // 2
+            # Get the mid point
+            mid = left + (right - left) // 2
 
-            # Check if we found the target
             if nums[mid] == target:
                 return mid
 
-            # if target is less than the middle element, search the left half:
+            # if target is less than mid, search through left half
             if nums[mid] > target:
                 right = mid - 1
 
-            # if target is greater than the middle element, search the right half.
+            # else, search through right half
             else:
                 left = mid + 1
 
