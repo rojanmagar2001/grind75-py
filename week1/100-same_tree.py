98146daca11c16705c5d794efcb76dfb744dd47e
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-05-09 14:30:10 +0545

    added same tree soln

---
+++ week1/100-same_tree.py
@@ -0,0 +1,44 @@
+# 100. Same Tree
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+from typing import Optional
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Iterative Approach
+        queue = deque([(p, q)])
+
+        while queue:
+            node1, node2 = queue.popleft()
+
+            if not node1 and not node2:
+                continue
+
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+
+        return True
+
+        # Recursive approach
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
